bda8b5e710f9ff5f386f1f4f4b115f8bc11dda5f
Nazarick0912 <gooyongshen12@gmail.com>
2025-12-17 23:12:44 +0800

    add the Navbar css, and create a new header file, add the background img to the header

--- Flux_Digital/src/App.jsx
+++ Flux_Digital/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='w-full overflow-hidden'>
+
+      <Header/>
     </div>
   )
 }
--- Flux_Digital/src/components/Navbar.jsx
+++ Flux_Digital/src/components/Navbar.jsx
@@ -4,14 +4,15 @@ import { assets } from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
-        <div>
-            <img src={assets.logo} alt="" />
-            <ul>
+        <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+            <img src={assets.logo} alt="" className="w-40 sm:w-52"/>
+            <ul className='hidden md:flex gap-7 text-white'>
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Services</a>
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Services</a>
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>Team</a>
             </ul>
+            <button className='hidden md:block bg-white px-6.5 py-2 rounded-full cursor-pointer'>Sign Up</button>
         </div>
     </div>
   )
